327de9d23cb8917ca15f117228078198cd7f6cfc
100griffs <105128698+100griffs@users.noreply.github.com>
2023-03-04 13:39:49 +0300

    deployment

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import  './Than.css'; 
 import { HelmetProvider } from 'react-helmet-async';
+import ReactGA from "react-ga4";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-BDT5NXKGB2");
 root.render(
   <React.StrictMode>
 
@@ -18,8 +20,14 @@ root.render(
  
   </React.StrictMode>
 );
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
